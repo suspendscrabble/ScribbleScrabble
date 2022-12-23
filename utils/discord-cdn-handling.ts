59365c5b0c234eae1d9ad7e7febcce918cbd3a08
Arcana <untruth-brink@mail.plural.moe>
2022-12-23 04:53:57 -0500

    Support animated image formats ...when hosted on Discord's CDN. Filetypes newly ignored by the rewrite to the media cache: * GIF * WebP * APNG

--- utils/discord-cdn-handling.ts
+++ utils/discord-cdn-handling.ts
@@ -1,18 +1,25 @@
 const discordCDNAttachmentRegex =
-  /^https:\/\/cdn\.discordapp\.com\/attachments\/([^?]+)/i
+  /^https:\/\/cdn\.discordapp\.com\/attachments\/(?<basename>.+?)(?<extension>\.[^?]+)/i
 
 const resizeMedia = (
   mediaURL: string,
   dimensions?: number[],
   format?: string
-) =>
-  mediaURL.replace(
-    discordCDNAttachmentRegex,
-    `https://media.discordapp.net/attachments/$1?width=${
-      dimensions?.[0] ?? 256
-    }&height=${dimensions?.[1] ?? dimensions?.[0] ?? 256}&format=${
-      format ?? 'webp'
-    }`
-  )
+) => {
+  const match = mediaURL.match(discordCDNAttachmentRegex)
+
+  if (!match?.groups?.extension.match(/\.(gif|webp|apng)/)) {
+    mediaURL = mediaURL.replace(
+      discordCDNAttachmentRegex,
+      `https://media.discordapp.net/attachments/$<basename>$<extension>?width=${
+        dimensions?.[0] ?? 256
+      }&height=${dimensions?.[1] ?? dimensions?.[0] ?? 256}&format=${
+        format ?? 'webp'
+      }`
+    )
+  }
+
+  return mediaURL
+}
 
 export { resizeMedia }
